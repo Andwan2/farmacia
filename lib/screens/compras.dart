8ac103cea94794ba0ef9c534f6163c4863ced5db
Inkling412 <enriqueu734@gmail.com>
2025-11-19 17:30:02 -0600

    Agregado archivos de interfaces

---
+++ lib/screens/compras.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ComprasScreen extends StatelessWidget {
+  const ComprasScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text("Pantalla de compras"));
+  }
+}
---
+++ lib/screens/productos.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+class ProductosScreen extends StatelessWidget {
+  const ProductosScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text("Pantalla de productos"));
+  }
+}
