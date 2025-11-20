bd77294286461b780bcf5dca09132e1ce0c491dd
Miguel Hernández Gaitan <miguelhernandezgaitan13@gmail.com>
2025-11-19 20:56:32 -0600

    fixed macacos auth

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -40,6 +40,7 @@ class _LoginScreenState extends State<LoginScreen> {
         });
       }
     } catch (error) {
+      print(error);
       setState(() {
         errorMessage = 'Credenciales incorrectas o usuario no encontrado';
       });
