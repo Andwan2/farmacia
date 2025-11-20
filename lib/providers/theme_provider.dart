c3218f3bda31b4da7e7e854b00efc6e176f0d829
Inkling412 <enriqueu734@gmail.com>
2025-11-20 00:14:06 -0600

    persistencia de tema con shared_preferences

--- lib/providers/theme_provider.dart
+++ lib/providers/theme_provider.dart
@@ -1,19 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ThemeProvider extends ChangeNotifier {
   bool _isDarkMode = false;
+  static const String _themeKey = 'isDarkMode';
+
+  ThemeProvider() {
+    _loadThemePreference();
+  }
 
   bool get isDarkMode => _isDarkMode;
 
   ThemeMode get themeMode => _isDarkMode ? ThemeMode.dark : ThemeMode.light;
 
+  // Cargar preferencia guardada
+  Future<void> _loadThemePreference() async {
+    final prefs = await SharedPreferences.getInstance();
+    _isDarkMode = prefs.getBool(_themeKey) ?? false;
+    notifyListeners();
+  }
+
+  // Guardar preferencia
+  Future<void> _saveThemePreference(bool value) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setBool(_themeKey, value);
+  }
+
   void toggleTheme() {
     _isDarkMode = !_isDarkMode;
+    _saveThemePreference(_isDarkMode);
     notifyListeners();
   }
 
   void setTheme(bool isDark) {
     _isDarkMode = isDark;
+    _saveThemePreference(isDark);
     notifyListeners();
   }
 }
